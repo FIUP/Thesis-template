a701e958cf97ddb8ca47e953df6c569410eeee3b
Alberto Lazari <alberto.lazari@gmail.com>
2022-06-28 14:01:52 +0200

    More efficiency optimizations

--- compile.sh
+++ compile.sh
@@ -12,6 +12,7 @@ loadParams() {
   BIBLIOGRAPHY=true
   CLEAN=true
   LOG=false
+  FULL_RUN=false
 
   # Check that last parameter is not an option
   if [[ $# -gt 0 && ${!#} =~ ^[^-].*$ ]]; then
@@ -20,12 +21,13 @@ loadParams() {
     PDF_NAME=$THESIS
   fi
 
-  while getopts :bcd:glh flag
+  while getopts :bcd:fglh flag
   do
     case "${flag}" in
       b) BIBLIOGRAPHY=false;;
       c) CLEAN=false;;
       d) BUILD_DIR=$OPTARG;;
+      f) FULL_RUN=true;;
       g) GLOSSARY=false;;
       l) LOG=true;;
       h) printHelp; exit 0;;
@@ -36,11 +38,13 @@ loadParams() {
 
 printHelp() {
   echo "LaTeX FIUP thesis template compile script"
-  echo "Usage:    compile.sh [-bcghl] [-d build_directory] [pdf_name]"
+  echo "Usage:    compile.sh [-bcfghl] [-d build_directory] [pdf_name]"
   echo "Shell options:"
   echo "    -b    skip bibliography compile"
   echo "    -c    skip build directory clean"
   echo "    -d    directory to put temporary files in"
+  echo "    -f    force the full compile process."
+  echo "          Comes handy if you have problems with glossary or bibligraphy"
   echo "    -g    skip glossary compile"
   echo "    -l    keep logs in build/ directory"
   echo "    -h    print this message"
@@ -61,17 +65,18 @@ makeBibliography() {
 }
 
 compileWithBibGloss() {
-  if [ $GLOSSARY = true ]; then
+  if [[ $GLOSSARY == true ]]; then
     makeGlossary
   fi
 
-  if [ $BIBLIOGRAPHY = true ]; then
+  if [[ $BIBLIOGRAPHY == true ]]; then
     makeBibliography
   fi
 
   compile
 
-  if [ $GLOSSARY = true ]; then
+  # The first time the glossary needs to be compiled three times
+  if [[ $FULL_RUN == true ]]; then
     makeGlossary
     compile
 
@@ -83,7 +88,7 @@ compileWithBibGloss() {
 clean() {
   rm $BUILD_DIR/*.{acn,aux,bbl,bcf,glo,ist,lof,lot,run.xml,toc} &> /dev/null
 
-  if [ $LOG = false ]; then
+  if [[ $LOG == false ]]; then
     rm $BUILD_DIR/*.{blg,log} &> /dev/null
   fi
 }
@@ -94,17 +99,21 @@ main() {
   
   loadParams "$@"
 
-  if [ ! -d $BUILD_DIR ]; then
+  if [[ ! -d $BUILD_DIR ]]; then
+    FULL_RUN=true
+    GLOSSARY=true
+    BIBLIOGRAPHY=true
+
     mkdir $BUILD_DIR;
   fi
 
   compile
 
-  if [ $GLOSSARY = true ] || [ $BIBLIOGRAPHY = true ]; then
+  if [[ $GLOSSARY == true ]] || [[ $BIBLIOGRAPHY == true ]]; then
     compileWithBibGloss
   fi
  
-  if [ $CLEAN = true ]; then
+  if [[ $CLEAN == true ]]; then
     clean
   fi
 

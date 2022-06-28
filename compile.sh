7521fc77d7baa156d5cd60d007e976d2e81dde01
Alberto Lazari <alberto.lazari@gmail.com>
2022-06-28 11:26:55 +0200

    Custum pdf name support added

--- compile.sh
+++ compile.sh
@@ -1,26 +1,33 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ### Params ###
 # LaTeX build files directory
-BUILD="build"
+BUILD_DIR="build"
 
 # Thesis root .tex file name
 THESIS="tesi"
 
 loadParams() {
-  glossary=true
-  bibliography=true
-  clean=true
-  log=false
+  GLOSSARY=true
+  BIBLIOGRAPHY=true
+  CLEAN=true
+  LOG=false
+
+  # check that last parameter is not an option
+  if [[ $# -gt 0 && ! ${!#} =~ -.* ]]; then
+    PDF_NAME=${!#}
+  else
+    PDF_NAME=$THESIS
+  fi
 
   while getopts :bcd:glh flag
   do
     case "${flag}" in
-      b) bibliography=false;;
-      c) clean=false;;
-      d) BUILD=$OPTARG;;
-      g) glossary=false;;
-      l) log=true;;
+      b) BIBLIOGRAPHY=false;;
+      c) CLEAN=false;;
+      d) BUILD_DIR=$OPTARG;;
+      g) GLOSSARY=false;;
+      l) LOG=true;;
       h) printHelp; exit 0;;
       *) printHelp; exit 1;;
     esac
@@ -29,7 +36,7 @@ loadParams() {
 
 printHelp() {
   echo "LaTeX FIUP thesis compile script"
-  echo "Usage:    compile.sh [-bdghl]"
+  echo "Usage:    compile.sh [-bcdghl] [pdf_name]"
   echo "Shell options:"
   echo "    -b    skip bibliography compile"
   echo "    -c    skip build directory clean"
@@ -40,40 +47,40 @@ printHelp() {
 }
 
 compile() {
-  pdflatex -output-directory=$BUILD -interaction=nonstopmode $THESIS 
+  pdflatex -output-directory=$BUILD_DIR -interaction=nonstopmode $THESIS 
 }
 
 makeGlossary() {
   makeindex -s $THESIS.ist -t $THESIS.glg -o $THESIS.{gls,glo}
-  makeglossaries -d $BUILD $THESIS
+  makeglossaries -d $BUILD_DIR $THESIS
   makeindex -s $THESIS.ist -t $THESIS.alg -o $THESIS.{acr,acn}
 }
 
 makeBibliography() {
-  biber --input_directory $BUILD --output_directory $BUILD $THESIS
+  biber --input_directory $BUILD_DIR --output_directory $BUILD_DIR $THESIS
 }
 
 compileWithBibGloss() {
-  if [ $glossary = true ]; then
+  if [ $GLOSSARY = true ]; then
     makeGlossary
   fi
 
-  if [ $bibliography = true ]; then
+  if [ $BIBLIOGRAPHY = true ]; then
     makeBibliography
   fi
 
   compile
 
-  if [ $glossary = true ]; then
+  if [ $GLOSSARY = true ]; then
     compile
   fi
 }
 
 clean() {
-  rm $BUILD/*.{acn,aux,bbl,bcf,glo,ist,lof,lot,run.xml,toc} &> /dev/null
+  rm $BUILD_DIR/*.{acn,aux,bbl,bcf,glo,ist,lof,lot,run.xml,toc} &> /dev/null
 
-  if [ $log = false ]; then
-    rm $BUILD/*.{blg,log} &> /dev/null
+  if [ $LOG = false ]; then
+    rm $BUILD_DIR/*.{blg,log} &> /dev/null
   fi
 }
 
@@ -81,24 +88,24 @@ main() {
   # Move to script directory
   cd $PWD/$( dirname -- "$0" )
   
-  loadParams $*
+  loadParams "$@"
 
-  if [ ! -d $BUILD ]; then
-    mkdir $BUILD;
+  if [ ! -d $BUILD_DIR ]; then
+    mkdir $BUILD_DIR;
   fi
 
   compile
 
-  if [ $glossary = true ] || [ $bibliography = true ]; then
+  if [ $GLOSSARY = true ] || [ $BIBLIOGRAPHY = true ]; then
     compileWithBibGloss
   fi
  
-  if [ $clean = true ]; then
+  if [ $CLEAN = true ]; then
     clean
   fi
 
   # Move generated pdf to the same level of the source file
-  mv $BUILD/$THESIS.pdf ./
+  mv $BUILD_DIR/$THESIS.pdf ./$PDF_NAME.pdf
 }
 
-main $*
+main "$@"
